0c39ba7ed92a62a4bef2f780c51ef9ebddfd1e89
Lucas Erlacher Rodrigues <lucaserlacher@hotmail.com>
2019-08-18 16:45:29 -0300

    Implementação tela login

--- web-client/browser-client/src/pages/Login.js
+++ web-client/browser-client/src/pages/Login.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './Login.css';
 
-export default function Login(){
-    return(
-      <h1>Olá mundo</h1>
-    );
+import api from '../services/api';
+
+export default function Login({history}){
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmit(e){
+    e.preventDefault();
+
+    const response = await api.post('/login', 
+    {
+        username,
+        password,
+    });
+
+    const { _id } = response.data;
+
+    history.push(`/dev/${_id}`);
+  }
+
+  return(
+    <div className="login-container">        
+      <form>
+        <strong>E-mail:</strong>
+        <input 
+        placeholder="Insira seu e-mail"
+        value={username}
+        onChange={e => setUsername(e.target.value)}></input>
+        
+        <strong>Senha:</strong>
+        <input 
+        type="password"
+        placeholder="Insira sua senha"
+        value={password}
+        onChange={e => setPassword(e.target.value)}></input>
+        
+        <div className="buttons">
+          <button 
+            type="submit"
+            onClick={e => handleSubmit(e)}>Entrar</button>
+          <button>Cadastrar</button>
+        </div>
+      </form>
+    </div>
+  );
 }
---
+++ web-client/browser-client/src/services/api.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:8000/'
+});
+
+export default api;
