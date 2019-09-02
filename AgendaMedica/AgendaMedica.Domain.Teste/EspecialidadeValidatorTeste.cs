6d6877f23fb88a9f333dd531c88acf2f249aa633
Danilo de Oliveira <danilo.oliveira@aevo.com.br>
2019-09-02 13:09:48 -0300

    Teste automatizado incluido

---
+++ AgendaMedica/AgendaMedica.Domain.Teste/EspecialidadeValidatorTeste.cs
@@ -0,0 +1,29 @@
+﻿using AgendaMedica.Domain.Entities;
+using AgendaMedica.Domain.Validations;
+using NUnit.Framework;
+
+namespace AgendaMedica.Domain.Teste
+{
+    [TestFixture]
+    public class EspecialidadeValidatorTeste
+    {
+        private EspecialidadeValidator validator;
+
+        [SetUp]
+        public void Setup()
+        {
+            validator = new EspecialidadeValidator();
+        }
+
+        [Test]
+        public void EspecialidadeCodigoObrigatorio()
+        {
+            Especialidade especialidade = new Especialidade
+            {
+                Codigo = string.Empty
+            };
+
+            Assert.IsFalse(validator.Validate(especialidade).IsValid);
+        }
+    }
+}
