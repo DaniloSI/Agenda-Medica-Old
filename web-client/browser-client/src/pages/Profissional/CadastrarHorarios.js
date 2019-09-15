4614ffe6afb4633a4eb3a03752991f846e7a6a99
Lucas Erlacher Rodrigues <lucaserlacher@hotmail.com>
2019-09-14 22:52:01 -0300

    Inserção do componente Date Range Picker

--- web-client/browser-client/src/pages/Profissional/CadastrarHorarios.js
+++ web-client/browser-client/src/pages/Profissional/CadastrarHorarios.js
@@ -1,30 +1,68 @@
 import React, { useState } from 'react';
-
+import DateRangePicker from '@wojtekmaj/react-daterange-picker';
 
 export default function CadastrarHorarios() {
 
     const [form, setForm] = useState({
-        dataHoraInicio: "14/02/2020",
-        dataHoraFim: "14/02/2021",
+        dataHoraInicio: new Date(),
+        dataHoraFim: new Date(),
         horarios: [],
         profissionalId: 10
     })
 
-    function handleSubmit(e) {
+    function adicionarHorario(e) {
+        form.horarios.push({
+            diaSemana: 0,
+            horaInicio: "teste",
+            horaFim: "teste 2"
+        });
 
+        setForm(
+            {
+                ...form,
+                horarios: form.horarios
+            });
+
+        e.preventDefault();
+    }
+
+    function handleSubmit(e) {
+        console.log(form);
+        e.preventDefault();
     }
 
     return (
         <div>
             <h1>Cadastrar Jornada de Trabalho</h1>
+
             <form>
                 <div>
-                    <span>Nome: </span>
-                    <span>Data de Início: </span> <span>Data de Término: </span>
+                    <label>Nome: </label>
+                    <input type="text"></input>
+                    <br />
+
+                    <label>Período de Vigência: </label>
+                    <DateRangePicker onChange={data =>
+                        setForm(
+                            {
+                                ...form,
+                                dataHoraInicio: data != null ? data[0] : new Date(),
+                                dataHoraFim: data != null ? data[1] : new Date()
+                            }
+                        )
+                    }
+                        value={[form.dataHoraInicio, form.dataHoraFim]}
+                    ></DateRangePicker>
                 </div>
 
+
+                <div id="adicionarHorariosFields" className="border rounded-lg">
+                    <h3>Adicionar novo horário</h3>
+                    <button onClick={adicionarHorario}>Adicionar</button>
+                </div>
+
+
                 <div>
-                    <button>Adicionar</button>
                     <table className="table table-borderless">
                         <thead>
                             <tr>
@@ -42,8 +80,8 @@ export default function CadastrarHorarios() {
                                         <tr>
                                             <td>{horario.diaSemana}</td>
                                             <td>Segunda Feira</td>
-                                            <td>13:00</td>
-                                            <td>19:00</td>
+                                            <td>{horario.horaInicio}</td>
+                                            <td>{horario.horaFim}</td>
                                             <td><button>Editar</button><button>Deletar</button></td>
                                         </tr>
                                     )
@@ -53,24 +91,9 @@ export default function CadastrarHorarios() {
                     </table>
                 </div>
 
-                <button onClick={e => {
-
-                    form.horarios.push({
-                        diaSemana: 0,
-                        horaInicio: "teste",
-                        horaFim: "teste 2"
-                    });
-
-                    setForm(
-                    {
-                        ...form,
-                        horarios: form.horarios
-                    });
-                    e.preventDefault();
-                }}>Salvar</button>
+                <button onClick={handleSubmit}>Salvar</button>
                 <button>Cancelar</button>
             </form>
-
         </div>
     );
 }
