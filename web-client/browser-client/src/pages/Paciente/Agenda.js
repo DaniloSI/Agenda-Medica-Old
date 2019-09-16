7cee6115136063228e7c08d81f2a729abe838ba8
Danilo de Oliveira <danilo.oliveira@aevo.com.br>
2019-09-16 20:57:50 -0300

    Agenda de Paciente com alguns dados Hard Coded

--- web-client/browser-client/src/pages/Paciente/Agenda.js
+++ web-client/browser-client/src/pages/Paciente/Agenda.js
@@ -1,11 +1,77 @@
-import React from 'react'
+import React, { forwardRef }  from 'react'
 import Container from '@material-ui/core/Container';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import MaterialTable from 'material-table';
+import {
+    Search,
+    Clear,
+    FirstPage,
+    LastPage,
+    ChevronLeft,
+    ChevronRight,
+    ArrowUpward
+} from '@material-ui/icons';
+
+const tableIcons = {
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref} />)
+};
 
 export default function Agenda() {
+    const [state, setState] = React.useState({
+        columns: [
+            {
+                title: 'Profissional',
+                field: 'nome'
+            },
+            {
+                title: 'Data',
+                field: 'data',
+                type: 'date'
+            },
+            {
+                title: 'Horário de Início',
+                field: 'horarioInicio'
+            },
+            {
+                title: 'Horário de Fim',
+                field: 'horarioFim',
+                type: 'time'
+            }
+        ],
+        data: [
+            {
+                nome: 'Mehmet da Silva',
+                data: '16/09/2019',
+                horarioInicio: '15:30',
+                horarioFim: '16:30'
+            },
+            {
+                nome: 'Mehmet Soarez de Sousa',
+                data: '21/09/2019',
+                horarioInicio: '08:30', 
+                horarioFim: '10:30'
+            }
+        ],
+      });
     return (
         <Container maxWidth="lg">
-            <h1>Agenda</h1>
+            <MaterialTable
+                title="Agenda"
+                columns={state.columns}
+                data={state.data}
+                icons={tableIcons}
+                options={
+                    {
+                        paging: false
+                    }
+                }
+            />
         </Container>
     )
 }
