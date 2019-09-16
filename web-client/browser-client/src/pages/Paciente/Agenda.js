d90c4cb9c67463e75b6e41ffc6e04a7cab65d82d
Danilo de Oliveira <danilo.oliveira@aevo.com.br>
2019-09-16 19:12:08 -0300

    Página de Agenda melhor

---
+++ web-client/browser-client/src/pages/Paciente/Agenda.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+export default function Agenda() {
+    return (
+        <Container maxWidth="lg">
+            <h1>Agenda</h1>
+        </Container>
+    )
+}
--- web-client/browser-client/src/pages/Paciente/Home.js
+++ web-client/browser-client/src/pages/Paciente/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { logout } from '../../services/auth';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -21,6 +22,7 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import EventNoteIcon from '@material-ui/icons/EventNote';
 import SearchIcon from '@material-ui/icons/Search';
+import Agenda from './Agenda'
 
 const drawerWidth = 260;
 
@@ -182,7 +184,14 @@ export default function MenuAppBar({ history }) {
         </div>
         <Divider />
         <List>
-          <ListItem button key='Minha Agenda'>
+          <ListItem button key='Minha Agenda' onClick={
+            () => {
+              ReactDOM.render(
+                <Agenda />,
+                document.getElementById('pagina-paciente')
+              );
+            }
+          }>
             <ListItemIcon>
               <EventNoteIcon />
             </ListItemIcon>
@@ -203,29 +212,7 @@ export default function MenuAppBar({ history }) {
         })}
       >
         <div className={classes.drawerHeader} />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-          donec massa sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <div id="pagina-paciente" />
       </main>
     </div>
   );
