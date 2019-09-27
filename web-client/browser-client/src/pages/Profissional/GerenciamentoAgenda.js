28cf0c8d3c612198f98c2016088bd75438becd00
Danilo de Oliveira <danilo.oliveira@aevo.com.br>
2019-09-27 16:42:01 -0300

    Campos adicionais de Agenda

--- web-client/browser-client/src/pages/Profissional/GerenciamentoAgenda.js
+++ web-client/browser-client/src/pages/Profissional/GerenciamentoAgenda.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
@@ -6,12 +7,19 @@ import NavBarProfissional from './NavBarProfissional.js';
 import Box from '@material-ui/core/Box';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
+import TextField from '@material-ui/core/TextField';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardDatePicker
+} from '@material-ui/pickers';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 
 const useStyles = makeStyles(theme => ({
@@ -19,6 +27,9 @@ const useStyles = makeStyles(theme => ({
       width: '100%',
       maxWidth: 360,
       backgroundColor: theme.palette.background.paper,
+    },
+    paper: {
+        padding: theme.spacing(3),
     },
   }));
 
@@ -90,15 +101,62 @@ export default function GerenciamentoAgenda(props) {
             content={
                 <div>
                     <CssBaseline />
-                    <Container maxWidth="lg" style={{ height: '20vh' }}>
-                        
+                    <Container maxWidth="xl">
+                        <Box mb={5}>
+                            <Paper className={classes.paper}>
+                                <Typography variant="h5" component="h6">Agenda</Typography>
+                                <br />
+                                <form noValidate autoComplete="off" >
+                                    <Grid container spacing={3} justify="flex-start" direction="row">
+                                        <Grid item xs={4}>
+                                            <TextField
+                                                id="titulo"
+                                                label="Título"
+                                                width="100%"
+                                                fullWidth
+                                                // onChange={handleChange('name')}
+                                            />
+                                        </Grid>
+                                        <Grid item xs={4}>
+                                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                                <KeyboardDatePicker
+                                                    id="date-picker-data-inicio-vigencia"
+                                                    label="Início da Vigência"
+                                                    format="MM/dd/yyyy"
+                                                    fullWidth
+                                                    // value={selectedDate}
+                                                    onChange={() => console.log('Data alterada.')}
+                                                    KeyboardButtonProps={{
+                                                        'aria-label': 'change date',
+                                                    }}
+                                                />
+                                            </MuiPickersUtilsProvider>
+                                        </Grid>
+                                        <Grid item xs={4}>
+                                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                                <KeyboardDatePicker
+                                                    id="date-picker-data-fim-vigencia"
+                                                    label="Fim da Vigência"
+                                                    format="MM/dd/yyyy"
+                                                    fullWidth
+                                                    // value={selectedDate}
+                                                    onChange={() => console.log('Data alterada.')}
+                                                    KeyboardButtonProps={{
+                                                        'aria-label': 'change date',
+                                                    }}
+                                                />
+                                            </MuiPickersUtilsProvider>
+                                        </Grid>
+                                    </Grid>
+                                </form>
+                            </Paper>
+                        </Box>
                     </Container>
-                    <br />
                     <Container maxWidth="xl">
                         <Box width="100%" height={100} display="flex" alignItems="flex-start">
                             {diasSemana.map((diaSemana, index) => 
-                                <Box width={1 / diasSemana.length} p={1} display="inline-block" maxHeight={5}>
-                                    <List subheader={<ListSubheader>{diaSemana}</ListSubheader>} className={classes.root} bgcolor="background.paper" key={index}>
+                                <Box width={1 / diasSemana.length} p={1} display="inline-block" maxHeight={5} key={index}>
+                                    <List subheader={<ListSubheader>{diaSemana}</ListSubheader>} className={classes.root} bgcolor="background.paper">
                                         {horarios.filter(h => h.diaSemana == index)
                                             .map(h => 
                                                 <ListItem key={h.horarioId}>
